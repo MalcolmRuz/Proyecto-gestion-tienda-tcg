b518f12108d10f1bb05f5f9ddacd769b07c11e95
Malcolm Ruz <malc.ruz@duocuc.cl>
2026-04-25 20:23:15 -0400

    Creacion clase carrito en microservicio 'Carrito'- Malcolm Ruz

---
+++ carrito/carrito/src/main/java/com/gestion_tienda_tcg/carrito/model/Carrito.java
@@ -0,0 +1,13 @@
+package com.gestion_tienda_tcg.carrito.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Carrito {
+    private char estado_carrito;
+}
