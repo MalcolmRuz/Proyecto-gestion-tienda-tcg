e13f9e842f568a011d65dcf3ff3886c182345f7a
MfJelta <os.torresr@duocuc.cl>
2026-06-12 16:13:56 -0400

    Feat :

--- productos/productos/src/main/java/com/gestion_tienda_tcg/productos/controller/v2/CategoriaControllerV2.java
+++ productos/productos/src/main/java/com/gestion_tienda_tcg/productos/controller/v2/CategoriaControllerV2.java
@@ -1,4 +1,15 @@
 package com.gestion_tienda_tcg.productos.controller.v2;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v2/categoria")
+@Tag(name = "CATEGORIA V2", description = "Control de categorias optimizada con HATEOAS")
 public class CategoriaControllerV2 {
 }
