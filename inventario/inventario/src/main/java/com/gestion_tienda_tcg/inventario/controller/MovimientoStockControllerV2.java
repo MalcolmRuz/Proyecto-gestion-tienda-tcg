60e00d9719fe53abc46f26e5d71d743253983571
MfJelta <os.torresr@duocuc.cl>
2026-06-11 20:51:00 -0400

    Feat : Creacion MovimientoStockControllerV2

---
+++ inventario/inventario/src/main/java/com/gestion_tienda_tcg/inventario/controller/MovimientoStockControllerV2.java
@@ -0,0 +1,14 @@
+package com.gestion_tienda_tcg.inventario.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/movimientosStock")
+@RequiredArgsConstructor
+@Tag(name = "Movimiento Stock V2", description = "Historial de cambios en el stock en Inventario")
+public class MovimientoStockControllerV2 {
+
+}
